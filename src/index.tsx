32a871f32c1f8aeba0887f956913bda68c3cb599
hckrg <harshagogia11@gmail.com>
2023-09-22 09:00:00 +0530

    added basic files

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+
+import App from 'App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
